8edf5192235b277c97c554aec9dbf4e64120bf4d
Andres <ayarceb@eafit.edu.co>
2020-12-24 08:30:35 -0500

    Metar_actualizacion

--- Metar/decode_metar.py
+++ Metar/decode_metar.py
@@ -1,50 +1,54 @@
 from metar import Metar
 import pandas as pd
+import os
 
-<<<<<<< HEAD
-a=['./CONSULTA METAR - IDEAM_SANTA_MARTA.csv','./CONSULTA METAR - IDEAM._Barranquilla.csv','./CONSULTA METAR - IDEAM_Barrancabermeja.csv','./CONSULTA METAR - IDEAM_BOGOTA.csv','./CONSULTA METAR - IDEAM_Cartagena.csv','./CONSULTA METAR - IDEAM_MEDELLIN.csv','./CONSULTA METAR - IDEAM_Monteria.csv','./CONSULTA METAR - IDEAM_Riohacha.csv','./CONSULTA METAR - IDEAM_Santiago_De_Cali.csv','./CONSULTA METAR - IDEAM_Valledupar.csv']
+path = os.getcwd()
 
-j=9
+a=['./CONSULTA METAR - Santa_Marta.csv','./CONSULTA METAR - Barranquilla.csv','./CONSULTA METAR - Barrancabermeja.csv','./CONSULTA METAR - BOGOTA.csv','./CONSULTA METAR - Cartagena.csv','./CONSULTA METAR - MEDELLIN.csv','./CONSULTA METAR - Monteria.csv','./CONSULTA METAR - Riohacha.csv','./CONSULTA METAR - Santiago_De_Cali.csv','./CONSULTA METAR - Valledupar.csv']
 
-df=pd.read_csv(a[j], sep=',',header=0)
-=======
-df=pd.read_csv('./Metar_archivos/CONSULTA METAR - IDEAM_SANTA_MARTA.csv', sep=',',header=0)
->>>>>>> 69e7ac40330c16eb9d735a946f883e3b5dcb17f0
+for j in range(0, len(a), 1):
+    print(j)
+    os.chdir(path)
+    df=pd.read_csv(a[j], sep=',',header=0)
 
-information = pd.DataFrame(columns=['D','T','P','DP'])
-for row in df.iterrows():
-    metar = row[1]['Informe']
-    index=metar.find('/')
-    if index==-1:
-        T=None
-        DP=None
-        P=None
-        print('Error al decodificar el METAR, "/" no encontrado')
-        information = information.append({'D': row[1]['Fecha hora'], 'T': T,'P': P, 'DP': DP}, ignore_index=True)
-        continue
-    if metar[index+4]=='A':
-        T=metar[index-2:index]
-        T=int(T)
-        DP=metar[index+1:index+3]
-        DP=int(DP)
-        P=metar[index+5:index+9]
-        if P[0]=='/':
+    information = pd.DataFrame(columns=['D','T','P','DP'])
+    for row in df.iterrows():
+        metar = row[1]['Informe']
+        index=metar.find('/')
+        if index==-1:
+            T=None
+            DP=None
             P=None
-            print('Error al decodificar el METAR, informacion de variable P faltante. Se encuentra "A////"')
+            print('Error al decodificar el METAR, "/" no encontrado')
             information = information.append({'D': row[1]['Fecha hora'], 'T': T,'P': P, 'DP': DP}, ignore_index=True)
             continue
-        P=P[0:2]+'.'+P[2:]
-        P=float(P)
-    else:
-        T=None
-        DP=None
-        P=None
-        print('Error al decodificar el METAR, formato no coincide')
-        information = information.append({'D': row[1]['Fecha hora'], 'T': T,'P': P, 'DP': DP}, ignore_index=True)
-        continue
+        if metar[index+4]=='A':
+            T=metar[index-2:index]
+            T=int(T)
+            DP=metar[index+1:index+3]
+            DP=int(DP)
+            P=metar[index+5:index+9]
+            if P[0]=='/':
+                P=None
+                print('Error al decodificar el METAR, informacion de variable P faltante. Se encuentra "A////"')
+                information = information.append({'D': row[1]['Fecha hora'], 'T': T,'P': P, 'DP': DP}, ignore_index=True)
+                continue
+            P=P[0:2]+'.'+P[2:]
+            P=float(P)
+        else:
+            T=None
+            DP=None
+            P=None
+            print('Error al decodificar el METAR, formato no coincide')
+            information = information.append({'D': row[1]['Fecha hora'], 'T': T,'P': P, 'DP': DP}, ignore_index=True)
+            continue
 
-    print(row[1]['Fecha hora'],T, P, DP)
-    information = information.append({'D': row[1]['Fecha hora'], 'T': T,'P': P, 'DP': DP}, ignore_index=True)
+        print(row[1]['Fecha hora'],T, P, DP)
+        information = information.append({'D': row[1]['Fecha hora'], 'T': T,'P': P, 'DP': DP}, ignore_index=True)
    
-    
-information.to_csv(r'./datos/datos.csv', index = True)
+
+    #os.chdir("/run/media/dirac/Datos/Reciente_Dropbox/users/arjo/lotos-euros/Repositorio_Personal_Slopez/Personal/Metar/Segunda_Fecha/datos")
+    os.chdir("/run/media/dirac/Datos/Reciente_Dropbox/users/arjo/lotos-euros/Repositorio_Personal_Slopez/Personal/Metar/Segunda_Fecha/datos_test")
+    information.to_csv(a[j], index = True)
+
+
