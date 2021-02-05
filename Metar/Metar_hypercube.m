2df29b241b1a8924dbaf957237e1c29554ac654d
jehinestrozar <jehinestrozar@unal.edu.co>
2021-02-05 06:30:43 -0500

    Actualizacion metar. dos periodos. Guardar cubos de periodos

--- Metar/Metar_hypercube.m
+++ Metar/Metar_hypercube.m
@@ -1,41 +1,64 @@
 % create metar hipercube solicitado
 % Dimensiones Dom_2    531 X 660   (nx,ny)
-% Simulation between August-18-2020_00:00:00 to
+% Simulation between 1. August-18-2020_00:00:00 to
 % August-21-2020_00:00:00 WRF time.
-
+% Simulation 2. Between september-1-2020_00:00:00 to september-4-2020_00:00:00
 clear
+clc
 
-% simulation date
-year        = 2020;
-start_month = 8;
-end_month   = 8;
-start_day   = 18;
-end_day     = 21;
-start_hour  = 00;
-end_hour    = 00;
-format_time ='yyyy-MM-dd HH:mm:ss';
-t1 = datetime(year,start_month,start_day,start_hour,0,0,'Format',format_time); 
-t2 = datetime(year,end_month,end_day,start_hour,0,0,'Format',format_time);
-date_hour = t1:hours(1):t2; date_hour=date_hour'; 
-day_simul = start_day:end_day-1;
-hour_simul=length(date_hour)-1; % Number of hour of the simulation.
 %
 %Extracción de latitudes y longitudes de salidas de WRF
 XLONG = ncread('/media/fermat/DD_JOE/FAC/corridas_validacion/Test_case_FAC_01_09_04_09/wrfout_d02_2020-09-04_00:00:00','XLONG');
 XLAT  = ncread('/media/fermat/DD_JOE/FAC/corridas_validacion/Test_case_FAC_01_09_04_09/wrfout_d02_2020-09-04_00:00:00','XLAT');
 %
+% seleccionar experimento: experimento=1; primer periodo, experimento=2; segundo periodo,
+experimento=2
+
+if experimento==1
+    % simulation date
+    year        = 2020;
+    start_month = 8;
+    end_month   = 8;
+    start_day   = 18;
+    end_day     = 21;
+    start_hour  = 00;
+    end_hour    = 00;
+    format_time ='yyyy-MM-dd HH:mm:ss';
+    t1 = datetime(year,start_month,start_day,start_hour,0,0,'Format',format_time); 
+    t2 = datetime(year,end_month,end_day,start_hour,0,0,'Format',format_time);
+    date_hour = t1:hours(1):t2; date_hour=date_hour'; 
+    day_simul = start_day:end_day-1;
+    hour_simul=length(date_hour)-1; % Number of hour of the simulation.
+    % Data directory. % Directorio con los datos METAR
+    mydir='/media/fermat/HomeDisk/fermat/FAC/repositorio_WRFDA_FAC/WRFDA_FAC_EAFIT/Metar/datos/1';
+else
+    % simulation date
+    year        = 2020;
+    start_month = 9; %8
+    end_month   = 9; %8
+    start_day   = 1; %18;
+    end_day     = 4; %21;
+    start_hour  = 00;
+    end_hour    = 00;
+    format_time ='yyyy-MM-dd HH:mm:ss';
+    t1 = datetime(year,start_month,start_day,start_hour,0,0,'Format',format_time); 
+    t2 = datetime(year,end_month,end_day,start_hour,0,0,'Format',format_time);
+    date_hour = t1:hours(1):t2; date_hour=date_hour'; 
+    day_simul = start_day:end_day-1;
+    hour_simul=length(date_hour)-1; % Number of hour of the simulation.
+    % Data directory. % Directorio con los datos METAR
+    mydir='/media/fermat/HomeDisk/fermat/FAC/repositorio_WRFDA_FAC/WRFDA_FAC_EAFIT/Metar/datos/2';
+end
+
 % Cubos para los datos segun numero de celdas del dominio, numero de
 % niveles y numero de horas de simulacion.
 Metar_cube_T=nan(531,660,41,hour_simul);%Temperature Metar Hipercube  Domain 2,dim_d02: 531,660,levels=41;time=73;
 Metar_cube_P=nan(531,660,41,hour_simul);%Pressure Metar Hipercube
 
-% Directorio con los datos METAR
-mydir='/media/fermat/HomeDisk/fermat/FAC/repositorio_WRFDA_FAC/WRFDA_FAC_EAFIT/Metar/datos';
-
 cd(mydir)
 % Ciudades con Datos usados.
-datos={'datos_valledupar.csv','datos_cali.csv','datos_SANTA_MARTA.csv','datos_rioacha.csv','datos_monteria.csv',...
-    'datos_Medellin.csv','datos_cartagena.csv','datos_Bogota.csv','datos_barrancabermeja.csv','datos_Barranquilla.csv'};
+datos={'datos_valledupar.csv','datos_cali.csv','datos_santa_marta.csv','datos_riohacha.csv','datos_monteria.csv',...
+    'datos_medellin.csv','datos_cartagena.csv','datos_Bogota.csv','datos_barrancabermeja.csv','datos_Barranquilla.csv'};
 %
 num_city=length(datos);
 %
@@ -71,3 +94,6 @@ for ind=1:num_city % ind:indice de ciudad según el orden dado a los datos.
 end
     Metar_cube_T(nx(ind,1),ny(ind,1),1,:)=temp; % Almacenar informacion en el cubo. Temperatura
     Metar_cube_P(nx(ind,1),ny(ind,1),1,:)=pres; % Almacenar informacion en el cubo. Presión
+
+    %save ('Metar_cube_simul_1','Metar_cube_T','Metar_cube_P')
+    %plot(squeeze( Metar_cube_T(nx(ind,1),ny(ind,1),1,:)))
